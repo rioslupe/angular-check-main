ad9da665b82502266c798b4cb05bfb9e49050adf
Lupe Rios <lupe.rios@masergy.com>
2023-06-29 15:56:49 -0500

    Implemented routing between tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatListModule} from "@angular/material/list";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {routes} from "./app.routes";
 
 @Component({
   selector: 'hd-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatListModule, RouterLink, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  routes = [...routes.filter((map)=> map.title)];
+}
